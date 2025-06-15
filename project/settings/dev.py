4b25798a2bc2dced83c0845b86e51dfa5656c0af
gabrieltrouve <gabrieltrouve5@gmail.com>
2025-06-15 12:29:42 +0200

    Add Tailwind CSS setup with PostCSS configuration and styles
    - Created package.json for theme with necessary scripts and dependencies for Tailwind CSS and PostCSS.
    - Added postcss.config.js to configure PostCSS plugins including Tailwind CSS, postcss-simple-vars, and postcss-nested.
    - Initialized styles.css to import Tailwind CSS and set up DaisyUI with a default theme, along with a source path for scanning templates and scripts.

---
+++ project/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-qh#_eopa%gz5!v&m(10lqb2nw-a(@!a#i%h8*lhk8pr$@_+srv"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
