a58ba3ba22f7b43a66c496c66009bf6299590829
gabrieltrouve <gabrieltrouve5@gmail.com>
2025-06-18 22:27:15 +0200

    Add CouncilMeeting model with author field and update index page template

--- councilmeeting/models.py
+++ councilmeeting/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
-from wagtail.fields import RichTextField, StreamField
-from wagtail.blocks import RichTextBlock
+from wagtail.fields import RichTextField
 from wagtail.documents.blocks import DocumentChooserBlock
-from wagtail.images.blocks import ImageChooserBlock
+from wagtail.snippets.models import register_snippet
+
+
+User = get_user_model()
 
 
 class CouncilMeetingIndexPage(Page):
@@ -14,17 +17,17 @@ class CouncilMeetingIndexPage(Page):
         FieldPanel("body"),
     ]
     
-    subpage_types = ["councilmeeting.CouncilMeetingPage"]
     max_count = 1
     
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
-        meetings = CouncilMeetingPage.objects.child_of(self).live().order_by('-date')
+        meetings = CouncilMeeting.objects.all().order_by('-date')
         context['meetings'] = meetings
         return context
 
 
-class CouncilMeetingPage(Page):
+@register_snippet
+class CouncilMeeting(models.Model):
     date = models.DateField("Date de la réunion")
     document = models.ForeignKey(
         'wagtaildocs.Document',
@@ -32,11 +35,24 @@ class CouncilMeetingPage(Page):
         related_name='+',
         verbose_name="Compte rendu de la réunion",
     )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        related_name='council_meetings',
+        verbose_name="Auteur",
+        null=True
+    )
 
-    content_panels = Page.content_panels + [
+    panels = [
         FieldPanel("date"),
         FieldPanel("document"),
+        FieldPanel("author"),
     ]
     
-    parent_page_types = ["councilmeeting.CouncilMeetingIndexPage"]
-    subpage_types = []
+    class Meta:
+        verbose_name = "Réunion du conseil municipal"
+        verbose_name_plural = "Réunions du conseil municipal"
+        ordering = ['-date']
+    
+    def __str__(self):
+        return f"Réunion du {self.date.strftime('%d/%m/%Y')}"
